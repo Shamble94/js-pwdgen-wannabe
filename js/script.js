5c76725dc80ea5e74f84b49e0746619c9aaee2ae
Shamble94 <fratus.nicholas@gmail.com>
2023-11-17 13:38:31 +0100

    Esercizio finito

---
+++ js/script.js
@@ -0,0 +1,6 @@
+
+let name = prompt ("Inserisci il tuo nome");
+let cognome = prompt ("Inserisci il tuo cognome");
+let colore = prompt ("Inserisci il tuo colore preferito");
+
+document.getElementById(`password`).innerHTML = `${name}${cognome}${colore}23`
